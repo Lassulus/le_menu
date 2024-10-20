290669c1fc8609bf7f1ccfaa567315e7ce21e2eb
lassulus <git@lassul.us>
2024-10-20 13:28:58 +0200

    add some runners and a nixos type

---
+++ examples/example.nix
@@ -0,0 +1,7 @@
+{
+  vim.run = "nix run github:lassulus/superconfig#nvim";
+  emojis.submenu = {
+    pizza.run = "echo pizza";
+    toast.run = "echo toast";
+  };
+}
--- flake.nix
+++ flake.nix
@@ -13,7 +13,10 @@
   outputs =
     inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } (
-      { lib, ... }:
+      { self, lib, ... }:
+      let
+        runners = lib.attrNames (builtins.readDir ./bin);
+      in
       {
         imports = [ ./treefmt.nix ];
         systems = [
@@ -24,18 +27,17 @@
           "x86_64-darwin"
           "aarch64-darwin"
         ];
+        flake.lib.types = import ./lib/types.nix { inherit lib; };
         flake.nixosModules = {
           le_menu =
             { ... }:
             {
               options = {
                 runners = lib.mkOption {
-                  type = lib.types.listOf lib.types.enum [
-                    "dmenu"
-                    "rofi"
-                  ];
+                  type = lib.types.listOf lib.types.enum runners;
                   default = [ ];
                 };
+                menu = self.lib.types.menuType;
               };
             };
         };
